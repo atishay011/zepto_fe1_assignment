c0091a3d9570e9e55623b7b136d981da506051c8
Atishay Jain <atishayjain690@gmail.com>
2024-01-18 13:58:38 +0530

    Added more mock data in USERS_LIST

--- src/constants/mock_data.js
+++ src/constants/mock_data.js
@@ -6,6 +6,16 @@ const USERS_LIST = [
     { name: 'Narendra Modi', email: 'narendramodi@gmail.com', avatar: UserAvatar },
     { name: 'Nelson Mandela', email: 'nelsonmandela@gmail.com', avatar: UserAvatar },
     { name: 'Barack Obama', email: 'b.obama@gmail.com', avatar: UserAvatar },
+    { name: 'Alex Johnson', email: 'alex.johnson@example.com', avatar: UserAvatar },
+    { name: 'Emma Brown', email: 'emma.brown@example.com', avatar: UserAvatar },
+    { name: 'Michael Smith', email: 'michael.smith@example.com', avatar: UserAvatar },
+    { name: 'Sophia Williams', email: 'sophia.williams@example.com', avatar: UserAvatar },
+    { name: 'Daniel Garcia', email: 'daniel.garcia@example.com', avatar: UserAvatar },
+    { name: 'Isabella Martinez', email: 'isabella.martinez@example.com', avatar: UserAvatar },
+    { name: 'David Lee', email: 'david.lee@example.com', avatar: UserAvatar },
+    { name: 'Mia Anderson', email: 'mia.anderson@example.com', avatar: UserAvatar },
+    { name: 'Ethan Young', email: 'ethan.young@example.com', avatar: UserAvatar },
+    { name: 'Olivia Hernandez', email: 'olivia.hernandez@example.com', avatar: UserAvatar }
 ]
 
 export default USERS_LIST;
